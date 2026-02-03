6b21fac08e06956d2b2a0a985d1de6f015aaece8
kikicodes23 <00117420@uca.edu.sv>
2026-02-03 00:04:58 -0600

    feat: modify email body

--- restapi/resources/views/emails/transcript_body.blade.php
+++ restapi/resources/views/emails/transcript_body.blade.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
 <head>
-    <title>Academic Transcript</title>
+    <meta charset="UTF-8">
+    <title>Constancia de Notas</title>
 </head>
 <body>
-    <p>Dear {{ $studentName }},</p>
+    <p>Estimado/a {{ $studentName }},</p>
 
-    <p>Please find attached your official Academic Transcript in PDF format.</p>
+    <p>Adjunto encontrará su Constancia de Notas oficial en formato PDF.</p>
 
-    <p>If you have any questions regarding your grades, please contact the administration.</p>
+    <p>Si tiene alguna duda con respecto a sus calificaciones, por favor póngase en contacto con la administración académica.</p>
 
     <br>
-    <p>Best regards,</p>
-    <p><strong>University Administration</strong></p>
+    <p>Atentamente,</p>
+    <p><strong>Administración Académica</strong></p>
 </body>
 </html>
